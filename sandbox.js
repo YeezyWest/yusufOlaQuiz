f8a24fe04b60e7d8ab84fb953eb7b5751fd2f7d6
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2022-12-29 14:44:02 +0100

    deploying my quiz website

---
+++ sandbox.js
@@ -0,0 +1,399 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // const title = 'Head Master';
+// // // // // // // // // // // // // // // // // // // // // // // // // // const author = 'West';
+// // // // // // // // // // // // // // // // // // // // // // // // // // const likes = '70';
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // let result = `The award for   ${title} on the book name ${author} with over ${likes}`;
+// // // // // // // // // // // // // // // // // // // // // // // // // // console.log(result)
+
+// // // // // // // // // // // // // // // // // // // // // // // // // const num = 39;
+// // // // // // // // // // // // // // // // // // // // // // // // // const num2 = 38;
+
+// // // // // // // // // // // // // // // // // // // // // // // // // let result = `the final award for ${title} on the book name ${author} with over $`
+
+
+// // // // // // // // // // // // // // // // // // // // // // // // let name = "Yusuf";
+// // // // // // // // // // // // // // // // // // // // // // // // let list = [ 'A' , 'B' , 'C' , 'D'];
+
+// // // // // // // // // // // // // // // // // // // // // // // // let result = list.includes('B'); 
+// // // // // // // // // // // // // // // // // // // // // // // // console.log(result);
+
+// // // // // // // // // // // // // // // // // // // // // // // const names = ["Ali" , "yusuf" , "Ade" , "ola"];
+
+// // // // // // // // // // // // // // // // // // // // // // // if ( names.length === 4 && names.includes("YU") ) {
+// // // // // // // // // // // // // // // // // // // // // // //     console.log("Yellloooo!!!!!!!!!")
+// // // // // // // // // // // // // // // // // // // // // // // }  else if ( names.includes("ola") ) {
+// // // // // // // // // // // // // // // // // // // // // // //     console.log("NAHHHHHHHHHH!!!!!!")
+// // // // // // // // // // // // // // // // // // // // // // // }
+
+// // // // // // // // // // // // // // // // // // // // // // const grade = 'F';
+
+// // // // // // // // // // // // // // // // // // // // // // switch(grade) {
+// // // // // // // // // // // // // // // // // // // // // //     case 'A': 
+// // // // // // // // // // // // // // // // // // // // // //     console.log("You got A!!");
+// // // // // // // // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // // // // // // // //     case 'B': 
+// // // // // // // // // // // // // // // // // // // // // //     console.log("You got B!!");
+// // // // // // // // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // // // // // // // //     case 'C': 
+// // // // // // // // // // // // // // // // // // // // // //     console.log("You got C!!");
+// // // // // // // // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // // // // // // // //     case 'D': 
+// // // // // // // // // // // // // // // // // // // // // //     console.log("You got D!!");
+// // // // // // // // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // // // // // // // //     case 'E': 
+// // // // // // // // // // // // // // // // // // // // // //     console.log("You got E!!");
+// // // // // // // // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // // // // // // // //     case 'F': 
+// // // // // // // // // // // // // // // // // // // // // //     console.log("You got F!!");
+// // // // // // // // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // // // // // // // //     default:
+// // // // // // // // // // // // // // // // // // // // // //         console.log("Invalid grade")
+// // // // // // // // // // // // // // // // // // // // // // }
+
+// // // // // // // // // // // // // // // // // // // // // function greet() {
+// // // // // // // // // // // // // // // // // // // // //     console.log("Yellllo!!");
+// // // // // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // // // // //greet()
+
+// // // // // // // // // // // // // // // // // // // // // const talk = function(name , message) {
+// // // // // // // // // // // // // // // // // // // // //     console.log(`hello ${name} ${message}`);
+// // // // // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // // // // talk("yusuf", "How are you doing?");
+
+// // // // // // // // // // // // // // // // // // // // const cal = function(radius){
+// // // // // // // // // // // // // // // // // // // //    return 5 * radius
+// // // // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // // // const area =  cal(5);
+// // // // // // // // // // // // // // // // // // // // console.log(area)
+
+
+// // // // // // // // // // // // // // // // // // // const aishaDay = ['waking up' , ' eating' , ' going to work']
+
+// // // // // // // // // // // // // // // // // // // for( let i = 0; i < aishaDay.length; i++ ){
+// // // // // // // // // // // // // // // // // // //     console.log(aishaDay[i])
+// // // // // // // // // // // // // // // // // // // }
+
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+// // // // // // // // // // // // // // // // // // // callAisha()
+
+// // // // // // // // // // // // // // // // // // const aishaDay = function(){
+// // // // // // // // // // // // // // // // // //    let day1 = ['waking up' , ' eating' , ' going to work']
+// // // // // // // // // // // // // // // // // //     for( let i = 0; i < day1.length; i++ ){
+// // // // // // // // // // // // // // // // // //   console.log(day1[i])
+// // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // aishaDay();
+
+// // // // // // // // // // // // // // // // // const greet = () => {
+// // // // // // // // // // // // // // // // //     return 'Hello, World!';
+// // // // // // // // // // // // // // // // // };
+
+// // // // // // // // // // // // // // // // // const print = greet();
+// // // // // // // // // // // // // // // // // console.log(print)
+
+// // // // // // // // // // // // // // // // // const bill = (products, tax) => {
+// // // // // // // // // // // // // // // // //     let total = 0;
+// // // // // // // // // // // // // // // // //     for(let i = 0; i < products.length; i++){
+// // // // // // // // // // // // // // // // //         total += products[i] + products[i] * tax;
+// // // // // // // // // // // // // // // // //     }
+// // // // // // // // // // // // // // // // //     return total;
+// // // // // // // // // // // // // // // // // }
+
+
+// // // // // // // // // // // // // // // // // console.log(bill([20,40,30], 5.5));
+
+// // // // // // // // // // // // // // // // let people = ['yusuf', 'olatunji', 'west', 'yeezy'];
+
+// // // // // // // // // // // // // // // // const logPerson = (element, index) => {
+// // // // // // // // // // // // // // // //     console.log(`${index} - Hello ${element}`);
+// // // // // // // // // // // // // // // // }
+
+// // // // // // // // // // // // // // // // people.forEach(logPerson);
+
+
+// // // // // // // // // // // // // // // let user = {
+// // // // // // // // // // // // // // //     name : 'Yusuf',
+// // // // // // // // // // // // // // //     age : 22,
+// // // // // // // // // // // // // // //     email : 'yusuf@gmail.com',
+// // // // // // // // // // // // // // //     location : 'kwara state',
+// // // // // // // // // // // // // // //     blogs : [
+// // // // // // // // // // // // // // //         { title : 'yo!! Was awesome' , likes : 30,},
+// // // // // // // // // // // // // // //         { title : 'completed' , likes : 30,}
+// // // // // // // // // // // // // // //     ],
+// // // // // // // // // // // // // // //     login: () => {
+// // // // // // // // // // // // // // //     console.log(`the user login`);
+// // // // // // // // // // // // // // //     },
+
+// // // // // // // // // // // // // // //     logBlog : function () {
+// // // // // // // // // // // // // // //       console.log(`the content of the blog :`);
+// // // // // // // // // // // // // // //       this.blogs.forEach(blog => {
+// // // // // // // // // // // // // // //         console.log(blog.title , blog.likes);
+// // // // // // // // // // // // // // //       });
+// // // // // // // // // // // // // // //     }
+// // // // // // // // // // // // // // // };
+
+// // // // // // // // // // // // // // // console.log(user);
+// // // // // // // // // // // // // // // console.log(user.age);
+
+// // // // // // // // // // // // // // //  user.age = 30
+// // // // // // // // // // // // // // //  console.log(user.age);
+
+// // // // // // // // // // // // // // //  console.log(user['email'])
+
+// // // // // // // // // // // // // // //  const email = user.email
+// // // // // // // // // // // // // // //  console.log(user.email)
+ 
+// // // // // // // // // // // // // // // user.login();
+
+// // // // // // // // // // // // // // // user.logBlog();
+
+// // // // // // // // // // // // // // const random = Math.random();
+
+// // // // // // // // // // // // // // console.log(random)
+// // // // // // // // // // // // // // console.log( Math.round(Math.random() * 100));  
+
+// // // // // // // // // // // // // const paras = document.querySelectorAll('p');
+
+// // // // // // // // // // // // // paras.forEach(element => {
+// // // // // // // // // // // // //     console.log(element.innerText);
+// // // // // // // // // // // // //     element.innerText += "-New text"
+// // // // // // // // // // // // // });
+
+// // // // // // // // // // // // // // paras.innerText = "-New paras";
+
+
+
+// // // // // // // // // // // // const list = document.querySelector('.content');
+
+// // // // // // // // // // // // const people = ['Yusuf','olatunji','West','Yeezy'];
+
+// // // // // // // // // // // // people.forEach(person => {
+// // // // // // // // // // // //   list.innerHTML += `<li>${person}</li>`
+// // // // // // // // // // // // });
+
+// // // // // // // // // // // const link = document.querySelector('a');
+
+// // // // // // // // // // // console.log(link.getAttribute('href'));
+
+// // // // // // // // // // // link.setAttribute('href', 'https://wwww.facebook.com');
+
+// // // // // // // // // // // console.log(link.getAttribute('href'));
+
+// // // // // // // // // // // const color = document.querySelector('p')
+
+// // // // // // // // // // // color.setAttribute('style', 'color:green');
+
+// // // // // // // // // // // const pick = document.querySelector('h1');
+
+// // // // // // // // // // // console.log(pick.style);
+
+// // // // // // // // // // // pick.style.marginLeft = '50px';
+// // // // // // // // // // // pick.style.color = 'purple';
+
+// // // // // // // // // // const content = document.querySelectorAll('p');
+
+// // // // // // // // // // content.forEach(element => {
+// // // // // // // // // //     if(element.innerText.includes('error')) {
+// // // // // // // // // //         element.classList.add('error');
+// // // // // // // // // //     } else if (element.innerText.includes('succes')){
+// // // // // // // // // //         element.classList.add('succes');
+// // // // // // // // // //     }
+// // // // // // // // // // });
+
+
+// // // // // // // // // const content = document.querySelectorAll('p');
+// // // // // // // // // content.forEach(element => {
+// // // // // // // // //     if( element.innerText.includes('succes')){
+// // // // // // // // //         element.classList.add('succes');
+// // // // // // // // //     } else if( element.innerText.includes('error') ){
+// // // // // // // // //         element.classList.add('error');
+// // // // // // // // //     }
+// // // // // // // // // });
+
+// // // // // // // // const article = document.querySelector('article')
+
+// // // // // // // // // console.log(article.children)
+// // // // // // // // // Array.from(article.children).forEach(element => {
+// // // // // // // // //     element.classList.add('plus');
+// // // // // // // // // });
+// // // // // // // const ul = document.querySelector('ul');
+
+// // // // // // // const button = document.querySelector('button');
+
+// // // // // // // button.addEventListener('click', () =>{
+// // // // // // //     const li = document.createElement('li');
+// // // // // // //     li.innerHTML = '<li class="click">Added something</li>';
+// // // // // // //     ul.prepend(li);
+   
+// // // // // // // })
+
+// // // // // // // const list = document.querySelectorAll('li');
+
+// // // // // // // list.forEach(element => {
+// // // // // // //    ul.addEventListener('click', (e) => {
+// // // // // // //     e.target.remove()
+// // // // // // //    }); 
+// // // // // // // });
+// // // // // // const ul = document.querySelector('ul');
+
+// // // // // // const button = document.querySelector('button')
+
+// // // // // // button.addEventListener('click' , ()=> {
+// // // // // //    const li = document.createElement('li');
+// // // // // //    li.innerHTML = "<li class = 'click'>something new </li>";
+// // // // // //    ul.prepend(li)
+// // // // // // });
+
+// // // // // // // const list = document.querySelectorAll('li');
+
+// // // // // // // list.forEach(item => {
+// // // // // // //     item.addEventListener('click' , (e) => {
+// // // // // // //         e.target.remove();
+// // // // // // //     })
+// // // // // // // });
+
+// // // // // // ul.addEventListener('click' , (e)=> {
+// // // // // //    if (e.target.tagName === 'LI'){
+// // // // // //     e.target.remove();
+// // // // // //    }
+// // // // // // });
+
+// // // // // const copy = document.querySelector('.copy');
+
+// // // // // copy.addEventListener('copy', () => {
+// // // // //     console.log("dnt do that")
+// // // // // })
+
+// // // const ul = document.querySelector('ul');
+
+// // // ul.addEventListener('click' , (e)=> {
+// // //         if (e.target.tagName === 'LI'){
+// // //          e.target.remove();
+// // //         }
+// // //     });
+
+// // // const button = document.querySelector('button');
+
+// // // button.addEventListener('click', ()=> {
+// // //     const li = document.createElement('li');
+// // //     li.innerHTML = "<li class = 'click'>New todo</li>"
+// // //     ul.prepend(li)
+// // // })
+
+// // // const pick = document.querySelector('.yo');
+
+// // // pick.addEventListener('mousemove', e => {
+// // //     pick.textContent = e.offsetX, e.offsetY
+// // // }); 
+
+// // // const button = document.querySelector('.Btn');
+// // // const content = document.querySelector('.content');
+// // // const close = document.querySelector('.close');
+
+// // // button.addEventListener('click', () => {
+// // //    content.style.display = 'block';  
+// // // });
+
+// // // close.addEventListener('click', () => {
+// // //     content.style.display = 'none';  
+// // //  });
+
+
+
+// //  const sign = document.querySelector('.signup-form');
+ 
+
+// // sign.addEventListener('submit', e => {
+// //    e.preventDefault();
+// //    console.log(sign.username.value)
+// // })
+
+// // const username = 'yusufo'
+// // const pathern = /^[a-z]{6,}$/
+
+// // let result = pathern.test(username);
+// // console.log(result);
+
+// const form = document.querySelector('.sign-up');
+// const message = document.querySelector('#message');
+// const email = document.querySelector('#email');
+// const usernamePattern = /^[a-zA-z]{6,}$/;
+// const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// form.addEventListener('submit', e => {
+//    e.preventDefault();
+
+//    //validation
+
+//   const username = form.username.value;
+//   const email = form.email.value;
+  
+
+//   if(usernamePattern.test(username) && emailPattern.test(email)) {
+//     message.textContent = 'Valid Username 😊🚀'
+//   } else {
+//    message.textContent = 'Invalid username 😂'
+//   }
+
+// });
+
+// //keyboard validation
+
+// form.username.addEventListener('keyup', e => {
+//    if(usernamePattern.test(e.target.value)) {
+//      form.username.setAttribute('class' , 'succes');
+//    } else { 
+//       form.username.setAttribute('class' , 'error');
+//    }
+// });
+
+
+// form.email.addEventListener('keyup', e => {
+//   if(emailPattern.test(e.target.value)) {
+//     form.email.setAttribute('class' , 'succes');
+//   } else { 
+//      form.email.setAttribute('class' , 'error');
+//   }
+// });
+
+const correctAnswer = ['A', 'B' , 'B' , 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+e.preventDefault();
+
+let score = 0;
+const userAnswers = [form.q1.value, form.q2.value , form.q3.value, form.q4.value];
+
+userAnswers.forEach((answer, index) => {
+if (answer === correctAnswer[index]) {
+  score += 25
+}
+});
+ 
+scrollTo(0,0)
+
+result.classList.remove('hidden');
+
+
+
+let output = 0;
+const timer = setInterval(() => {
+result.querySelector('span').textContent = `${output}%`;
+if(output === score){
+  clearInterval(timer);
+} else {
+  output++;
+}
+  
+}, 10);
+
+
+});

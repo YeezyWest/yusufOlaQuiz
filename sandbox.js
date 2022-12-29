94ed0c520916b13713cfe8a01083ff0ac1ffb2bc
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2022-12-29 15:39:45 +0100

    adding question 5 to the JS file

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const correctAnswer = ['A', 'B' , 'B' , 'B'];
+const correctAnswer = ['A', 'B' , 'B' , 'B' ,'A'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
@@ -6,11 +6,11 @@ form.addEventListener('submit', e => {
 e.preventDefault();
 
 let score = 0;
-const userAnswers = [form.q1.value, form.q2.value , form.q3.value, form.q4.value];
+const userAnswers = [form.q1.value, form.q2.value , form.q3.value, form.q4.value , form.q5.value];
 
 userAnswers.forEach((answer, index) => {
 if (answer === correctAnswer[index]) {
-  score += 25
+  score += 20
 }
 });
  
